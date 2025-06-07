db6304b97973b4ffc1efcb6f859b030ea2463932
dsntmattr <dsntmttr@gmx.de>
2025-06-07 22:02:05 +0200

    Renamed 02.1 and added study period.

--- scripts/02.1_get_modis_aggregate_P1M_mean.R
+++ scripts/02.1_get_modis_aggregate_P1M_mean.R
@@ -20,7 +20,6 @@ aoi  <- bbox.vector
 coll <- "modis-43A4-061"
 bds  <- c("Nadir_Reflectance_Band1","Nadir_Reflectance_Band2", "Nadir_Reflectance_Band3")
 dt   <- "P1M"
-out  <- "data/work/reference/P1M/"
 pre  <- 'MODIS_'
 
 
@@ -28,6 +27,12 @@ pre  <- 'MODIS_'
 # Load the data for each TOI,
 # aggregate the scenes to one picture for each month
 # and calculate the mean value.
+
+
+# Reference period. -------------------------------------------------------
+
+out  <- "data/work/reference/P1M/"
+
 get_data(aoi, toi = "2000-05-01/2000-09-30", coll, bds, dt, out, pre)
 get_data(aoi, toi = "2001-05-01/2001-09-30", coll, bds, dt, out, pre)
 get_data(aoi, toi = "2002-05-01/2002-09-30", coll, bds, dt, out, pre)
@@ -41,3 +46,14 @@ get_data(aoi, toi = "2009-05-01/2009-09-30", coll, bds, dt, out, pre)
 get_data(aoi, toi = "2010-05-01/2010-09-30", coll, bds, dt, out, pre)
 get_data(aoi, toi = "2011-05-01/2011-09-30", coll, bds, dt, out, pre)
 get_data(aoi, toi = "2012-05-01/2012-09-30", coll, bds, dt, out, pre)
+
+# Study period. -----------------------------------------------------------
+
+out  <- "data/work/study/P1M/"
+
+get_data(aoi, toi = "2013-05-01/2013-09-30", coll, bds, dt, out, pre)
+get_data(aoi, toi = "2014-05-01/2014-09-30", coll, bds, dt, out, pre)
+get_data(aoi, toi = "2015-05-01/2015-09-30", coll, bds, dt, out, pre)
+get_data(aoi, toi = "2016-05-01/2016-09-30", coll, bds, dt, out, pre)
+get_data(aoi, toi = "2017-05-01/2017-09-30", coll, bds, dt, out, pre)
+
